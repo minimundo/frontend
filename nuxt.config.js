50d17a9f13953ab91f56d62e81ea7ea33c968377
Kevin Almeida <janioklalmeida05@gmail.com>
2022-07-01 18:54:39 -0300

    🔧 chore: configuring authentication process in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,12 +37,28 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/auth',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://127.0.0.1:3333/api/',
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: 'signin/teacher', method: 'post', propertyName: 'token.token' },
+          user: false
+        }
+      }
+    },
+
+    redirect: {
+      home: '/teacher'
+    }
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
