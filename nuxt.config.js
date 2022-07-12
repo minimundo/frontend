4f0b05bd7aa4b9539d2bde31f7a83ce44e9a9d32
Kevin Almeida <janioklalmeida05@gmail.com>
2022-07-12 05:27:50 +0000

    🔧 chore: new project´s title (minimundo)

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: 'minimundo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -50,15 +50,19 @@ export default {
     strategies: {
       local: {
         endpoints: {
-          login: { url: 'signin/teacher', method: 'post', propertyName: 'token.token' },
-          user: false
-        }
-      }
+          login: {
+            url: 'signin/teacher',
+            method: 'post',
+            propertyName: 'token.token',
+          },
+          user: false,
+        },
+      },
     },
 
     redirect: {
-      home: '/teacher'
-    }
+      home: '/teacher',
+    },
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
