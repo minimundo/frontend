129267487908eb08cd6ba93e50c49aa2f712dd0e
Kevin Almeida <janioklalmeida05@gmail.com>
2022-07-15 04:24:09 -0300

    💄 feat: add new fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/layouts/stylesheet.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
