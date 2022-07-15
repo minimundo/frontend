88206ef15bc221a6b649706628fa6bdc5fff821f
Kevin Almeida <janioklalmeida05@gmail.com>
2022-07-15 04:48:17 -0300

    🔧 chore: adding api production web address

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:3333/api/',
+    baseURL: 'https://api-minimundo.herokuapp.com/api',
   },
 
   auth: {
