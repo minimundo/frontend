5390697ac7359f2e0adb988c463b78a52fd750f8
Kevin Almeida <janioklalmeida05@gmail.com>
2022-07-17 20:01:29 -0300

    🔧 chore: configuring directories for components, and styling files

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,14 +16,20 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/layouts/stylesheet.css'
+    '~/layouts/stylesheet.css',
+    '~/layouts/config.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      '~/components',
+      '~/components/navbar'
+    ],
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
