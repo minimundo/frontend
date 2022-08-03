60610e175d212a81a260eac64eecfa6d3bce4677
Kevin Almeida <janioklalmeida05@gmail.com>
2022-08-03 04:15:29 -0300

    🔧 chore: new plugin file, configuration vee-validate

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '~/plugins/vee-validate.js'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
@@ -81,5 +83,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['vee-validate']
+  },
 }
---
+++ plugins/vee-validate.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import { ValidationProvider, ValidationObserver, extend} from 'vee-validate'
+import { required, email } from 'vee-validate/dist/rules'
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+
+extend('required', {
+    ...required,
+    message: 'Este campo é obrigatório!'
+})
+
+extend('email', {
+    ...email,
+    message: 'O email inserido deve ser válido!'
+})
