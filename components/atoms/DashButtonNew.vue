129d1cbdc08326fc9564f4fff44186718b61a807
Kevin Almeida <janioklalmeida05@gmail.com>
2022-09-22 00:24:00 -0300

    ✨ feat: new atoms, form question item and dashboard button new/add

--- components/atoms/DashButtonNew.vue
+++ components/atoms/DashButtonNew.vue
@@ -1,19 +1,10 @@
 <template>
   <div class="dash-button-new">
-      <nuxt-link :to="$currentLink">
-        <button
-            type="button"
-            class="
-            btn btn-sm 
-            px-2
-            py-2
-            btn btn-light
-            "
-        >
-            
-            {{ $currentRoute }} <i class="fa-solid fa-circle-plus"></i>
-        </button>
-      </nuxt-link>
+    <nuxt-link :to="$currentLink">
+      <button type="button" class="btn btn-sm px-2 py-2 btn btn-light">
+        {{ $currentRoute }} <i class="fa-solid fa-circle-plus"></i>
+      </button>
+    </nuxt-link>
   </div>
 </template>
   <script>
@@ -22,30 +13,31 @@ export default {
     route: { type: String, required: true },
   },
   data() {
-    return {
-    }
+    return {}
   },
   computed: {
     $currentRoute() {
       if (this.route === '/dashboard/questions') {
         return 'Nova Questão'
-      } else if(this.route === '/dashboard/countries') {
+      } else if (this.route === '/dashboard/countries') {
         return 'Novo País'
-      } else if(this.route === '/dashboard/users') {
+      } else if (this.route === '/dashboard/users') {
         return 'Novo Usuário'
       } else {
         return ''
       }
     },
     $currentLink() {
-        if(this.route === '/dashboard/question') {
-            return '/dashboard/question/add'
-        } else if(this.route === '/dashboard/users') {
-            return '/dashboard/users/add'
-        } else {
-            return '/'
-        }
-    }
+      if (this.route === '/dashboard/questions') {
+        return '/dashboard/questions/add'
+      } else if (this.route === '/dashboard/countries') {
+        return '/dashboard/countries/add'
+      } else if (this.route === '/dashboard/users') {
+        return '/dashboard/users/add'
+      } else {
+        return ''
+      }
+    },
   },
 }
 </script>
---
+++ components/atoms/FormQuestionItem.vue
@@ -0,0 +1,120 @@
+<template>
+  <div v-if="type != 'button'" class="form-item form-group col-sm-6 col-12">
+    <div v-if="type === 'title'" class="h4 font-weight-bold">
+      {{ title }}
+      <small class="h6 form-text text-muted font-weight-semibold">
+        {{ description }}
+      </small>
+    </div>
+    <label
+      v-if="type != 'title' && type != 'button-cancel' && type != 'button-save'"
+      class="form-label"
+      :for="id"
+      >{{ title }}<span class="text-danger">*</span></label
+    >
+    <div class="form-floating">
+      <textarea
+        v-if="type === 'textarea'"
+        id="floatingTextarea"
+        class="form-control"
+        :placeholder="`Escreva aqui.`"
+      ></textarea>
+    </div>
+    <input
+      v-if="
+        type != 'textarea' &&
+        type != 'title' &&
+        type != 'select-correct' &&
+        type != 'select-grade' &&
+        type != 'button-cancel' &&
+        type != 'button-save'
+      "
+      :id="id"
+      :type="type"
+      class="form-control"
+    />
+    <select
+      v-if="type === 'select-correct'"
+      :id="id"
+      class="form-select btn btn-outline-secondary"
+    >
+      <option selected disabled></option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+    </select>
+    <select
+      v-if="type === 'select-grade'"
+      :id="id"
+      class="form-select btn btn-outline-secondary"
+    >
+      <option selected disabled></option>
+      <option value="6">6º ano</option>
+      <option value="7">7º ano</option>
+      <option value="8">8º ano</option>
+      <option value="9">9º ano</option>
+      <option value="1">1º ano (Médio)</option>
+      <option value="2">2º ano (Médio)</option>
+      <option value="3">3º ano (Médio)</option>
+    </select>
+    <small
+      v-if="type != 'title' && type != 'button-cancel' && type != 'button-save'"
+      class="form-text text-muted"
+    >
+      {{ description }}
+    </small>
+    <div>
+      <div v-if="type === 'button-cancel'" class="card-button">
+        <nuxt-link :id="id" to="/dashboard/questions" class="btn btn-sm px-2 py-2 btn">
+          {{ title }}
+        </nuxt-link>
+      </div>
+      <div v-if="type === 'button-save'" class="card-button">
+        <button
+          :id="id"
+          type="button"
+          class="btn btn-sm px-2 py-2 btn btn-dark"
+        >
+          {{ title }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    type: { type: String, required: true },
+    id: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, default: '' },
+  },
+}
+</script>
+<style scoped>
+label {
+  color: black;
+}
+
+small {
+  color: #77838f;
+}
+
+.h4 {
+  color: black;
+}
+
+.btn {
+  font-size: 1rem;
+}
+
+.btn-dark {
+  background-color: var(--primary-color);
+  transition: all 0.3s ease-in-out !important;
+}
+
+.btn-dark:hover {
+  transform: translateY(-2px) !important;
+}
+</style>
