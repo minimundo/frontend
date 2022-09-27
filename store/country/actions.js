feaee83640d52d97f8c84a129162d0d91e034883
Kevin Almeida <janioklalmeida05@gmail.com>
2022-09-27 09:23:56 -0300

    ✨ feat: new vuex functions, country

--- store/country/actions.js
+++ store/country/actions.js
@@ -1,18 +1,7 @@
 export default {
-    getCountry(context) {
-        return this.$axios.$get('/countries').then((response) => {
-            context.commit('STORE', response)
+    async index(context) {
+        return await this.$axios.$get('/countries').then((response) => {
+            context.commit('INDEX', response)
         })
     },
-
-    removeCountry(context, id) {
-        const userToken = this.$auth.strategy.$auth.$storage._state["_token.local"]
-        return this.$axios({
-            url: `/countries/${id}`,
-            method: 'DELETE',
-            headers: { 'Authorization': `Bearer ${userToken.replace('Bearer ', '')}`}
-        }).then(() => {
-            this.$router.go(0)
-        })
-    }
 }
--- store/country/mutations.js
+++ store/country/mutations.js
@@ -1,5 +1,15 @@
 export default {
+    INDEX(state, payload) {
+        state.countries = payload.sort((a, b) => {
+            if (a.name < b.name) {
+              return -1 
+            } else {
+              return true
+            }
+          })
+    },
+
     STORE(state, payload) {
-        state.countries = payload
+        state.countries.push(payload)
     }
 }
