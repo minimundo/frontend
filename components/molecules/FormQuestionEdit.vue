18a99c744e770bb69eb96a029840486207cd4037
Kevin Almeida <janioklalmeida05@gmail.com>
2022-10-02 15:22:41 -0300

    ✨ feat: new action for update question

--- components/molecules/FormQuestionEdit.vue
+++ components/molecules/FormQuestionEdit.vue
@@ -1,7 +1,82 @@
 <template>
-    <div class="form-dash">
-      <Container>
-        <div class="form-card space-bottom-1 p-5 card">
+  <div class="form-dash">
+    <Container>
+      <div class="form-card space-bottom-1 p-5 card">
+        <div
+          class="
+            title-form-question
+            d-flex
+            align-items-center
+            justify-content-center
+            text-center
+          "
+        >
+          <FormQuestionItem
+            id="title-form-question"
+            type="title"
+            title="Detalhes da Questão"
+            description="Você pode atualizar os dados da questão, inserindo novas informações nos campos."
+          />
+        </div>
+        <form v-if="question" ref="formQuestionRegister">
+          <FormQuestionItem
+            id="wording"
+            v-model="wording"
+            :value="wording"
+            type="textarea"
+            title="Enunciado"
+            description="Apresenta os detalhes e trás a pergunta a ser respondida."
+          />
+          <FormQuestionItem
+            id="answer1"
+            v-model="answer1"
+            type="textarea"
+            title="Alternativa 1"
+            description="Opção de resposta do aluno."
+          />
+          <FormQuestionItem
+            id="answer2"
+            v-model="answer2"
+            type="textarea"
+            title="Alternativa 2"
+            description="Opção de resposta do aluno."
+          />
+          <FormQuestionItem
+            id="answer3"
+            v-model="answer3"
+            type="textarea"
+            title="Alternativa 3"
+            description="Opção de resposta do aluno."
+          />
+          <FormQuestionItem
+            id="answer4"
+            v-model="answer4"
+            type="textarea"
+            title="Alternativa 4"
+            description="Opção de resposta do aluno."
+          />
+          <FormQuestionItem
+            id="select-correct-answer"
+            v-model="correctAnswer"
+            type="select-correct"
+            title="Selecione a alternativa correta"
+            description="Cada questão deve possuir somente 1 alternativa correta."
+          />
+          <FormQuestionItem
+            id="select-country"
+            v-model="country_id"
+            type="select-country"
+            :countries="$countriesPayload"
+            title="País"
+            description="Informe sobre qual país a questão se refere."
+          />
+          <FormQuestionItem
+            id="select-grade"
+            v-model="grade"
+            type="select-grade"
+            title="Série"
+            description="Informar qual a série escolar que a questão deve ser aplicada."
+          />
           <div
             class="
               title-form-question
@@ -11,166 +86,91 @@
               text-center
             "
           >
-            <FormQuestionItem
-              id="title-form-question"
-              type="title"
-              title="Detalhes da Questão"
-              description="Você pode atualizar os dados da questão, inserindo novas informações nos campos."
-            />
-          </div>
-          <form ref="formQuestionRegister">
-            <FormQuestionItem
-              id="wording"
-              v-model="wording"
-              :value="wording"
-              type="textarea"
-              title="Enunciado"
-              description="Apresenta os detalhes e trás a pergunta a ser respondida."
-            />
-            <FormQuestionItem
-              id="answer1"
-              v-model="answer1"
-              type="textarea"
-              title="Alternativa 1"
-              description="Opção de resposta do aluno."
-            />
-            <FormQuestionItem
-              id="answer2"
-              v-model="answer2"
-              type="textarea"
-              title="Alternativa 2"
-              description="Opção de resposta do aluno."
-            />
-            <FormQuestionItem
-              id="answer3"
-              v-model="answer3"
-              type="textarea"
-              title="Alternativa 3"
-              description="Opção de resposta do aluno."
-            />
-            <FormQuestionItem
-              id="answer4"
-              v-model="answer4"
-              type="textarea"
-              title="Alternativa 4"
-              description="Opção de resposta do aluno."
-            />
-            <FormQuestionItem
-              id="select-correct-answer"
-              v-model="correctAnswer"
-              type="select-correct"
-              title="Selecione a alternativa correta"
-              description="Cada questão deve possuir somente 1 alternativa correta."
-            />
-            <FormQuestionItem
-              id="select-country"
-              v-model="country_id"
-              type="select-country"
-              :countries="$countriesPayload"
-              title="País"
-              description="Informe sobre qual país a questão se refere."
-            />
-            <FormQuestionItem
-              id="select-grade"
-              v-model="grade"
-              type="select-grade"
-              title="Série"
-              description="Informar qual a série escolar que a questão deve ser aplicada."
-            />
-            <div
-              class="
-                title-form-question
-                d-flex
-                align-items-center
-                justify-content-center
-                text-center
-              "
-            >
-              <div class="button-cancel m-1">
-                <FormQuestionItem
-                  id="button-cancel"
-                  type="button-cancel"
-                  title="Cancelar"
-                />
-              </div>
-              <div class="button-save m-1" @click="addQuestion">
-                <FormQuestionItem
-                  id="button-save"
-                  type="button-save"
-                  title="Salvar"
-                />
-              </div>
+            <div class="button-cancel m-1">
+              <FormQuestionItem
+                id="button-cancel"
+                type="button-cancel"
+                title="Cancelar"
+              />
             </div>
-          </form>
-        </div>
-      </Container>
-      <Container> </Container>
-    </div>
-  </template>
+            <div class="button-save m-1" @click="updateQuestion">
+              <FormQuestionItem
+                id="button-save"
+                type="button-save"
+                title="Salvar"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </Container>
+    <Container> </Container>
+  </div>
+</template>
   <script>
-  import ToastMixin from '~/mixins/toastMixin'
-  
-  export default {
-    mixins: [ToastMixin],
-    props: {
-        question: { type: Object, required: true },
-    },
-    data() {
+import ToastMixin from '~/mixins/toastMixin'
+
+export default {
+  name: 'FormQuestionEdit',
+  mixins: [ToastMixin],
+  props: {
+    question: { type: Object, required: true },
+  },
+  data() {
+    return {
+      wording: this.question.wording,
+      answer1: this.question.answer1,
+      answer2: this.question.answer2,
+      answer3: this.question.answer3,
+      answer4: this.question.answer4,
+      correctAnswer: this.question.correct_answer,
+      grade: this.question.grade,
+      country_id: this.question.country.id,
+    }
+  },
+  computed: {
+    $dataPayload() {
       return {
-        wording: this.question.wording,
-        answer1: this.question.answer1,
-        answer2: this.question.answer2,
-        answer3: this.question.answer3,
-        answer4: this.question.answer4,
-        correctAnswer: this.question.correct_answer,
-        grade: this.question.grade,
-        country_id: this.question.country_id,
+        id: this.question.id,
+        grade: this.grade,
+        wording: this.wording,
+        answer1: this.answer1,
+        answer2: this.answer2,
+        answer3: this.answer3,
+        answer4: this.answer4,
+        correct_answer: this.correctAnswer,
+        country_id: this.country_id,
       }
     },
-    computed: {
-      $dataPayload() {
-        return {
-          grade: this.grade,
-          wording: this.wording,
-          answer1: this.answer1,
-          answer2: this.answer2,
-          answer3: this.answer3,
-          answer4: this.answer4,
-          correct_answer: this.correctAnswer,
-          country_id: this.country_id,
-        }
-      },
-      $countriesPayload() {
-        return this.$store.getters['country/index']
-      },
+    $countriesPayload() {
+      return this.$store.getters['country/index']
     },
-    created() {
-      this.$store.dispatch('country/index')
-      console.log(this.question)
-    },
-    methods: {
-      addQuestion() {
-        this.$store
-          .dispatch('question/create', this.$dataPayload)
-          .then(() => {
+  },
+  created() {
+    this.$store.dispatch('country/index')
+  },
+  methods: {
+    updateQuestion() {
+      this.$store
+        .dispatch('question/update', this.$dataPayload)
+        .then(() => {
+          this.showToastMixin(
+            'A questão foi atualizada!',
+            'Tudo Certo!',
+            'success'
+          )
+        })
+        .catch((err) => {
+          if (err.response.data) {
             this.showToastMixin(
-              'A questão foi cadastrada!',
-              'Tudo Certo!',
-              'success'
+              'Algo não ocorreu conforme o esperado.',
+              'Erro ao Atualizar!',
+              'danger'
             )
-            this.$refs.formQuestionRegister.reset()
-          })
-          .catch((err) => {
-            if (err.response.data) {
-              this.showToastMixin(
-                'Algo não ocorreu conforme o esperado.',
-                'Erro ao Excluir!',
-                'danger'
-              )
-            }
-          })
-      },
+          }
+        })
     },
-  }
-  </script>
+  },
+}
+</script>
   
--- store/question/actions.js
+++ store/question/actions.js
@@ -26,14 +26,18 @@ export default {
         })
     },
 
-    async update(context, payload, id) {
-        const userToken = this.$auth.strategy.$auth.$storage._state["_token.local"]
-        return await this.$axios.$put({
-            url: `/questions/${id}`,
-            headers: { 'Authorization': `Bearer ${userToken.replace('Bearer ', '')}` },
-            body: payload
-        }).then((response) => {
-            context.commit('STORE', response)
+    async update(context, payload) {
+        return await this.$axios.$put(`/questions/${payload.id}`, {
+            wording: payload.wording,
+            answer1: payload.answer1,
+            answer2: payload.answer2,
+            answer3: payload.answer3,
+            answer4: payload.answer4,
+            correct_answer: payload.correct_answer,
+            grade: payload.grade,
+            country_id: payload.country_id
+        }).then(() => {
+            context.dispatch('index');
         })
     },
 
