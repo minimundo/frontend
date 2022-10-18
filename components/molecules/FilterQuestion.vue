1c06412e053227792478a03495dd007cb4d984d6
Juan Nobert <nobert407@gmail.com>
2022-10-17 21:39:24 -0300

    :sparkles: feat: question filter form

---
+++ components/molecules/FilterQuestion.vue
@@ -0,0 +1,31 @@
+<template>
+   
+    <Container>
+        <div class="form-row">
+                <div class="col-7">
+                    <input type="text" class="form-control" placeholder="Digite um trecho da questão...">
+                </div>
+                <div class="col-2">
+                    <select id="pais" class="custom-select">
+                        <option value="" disabled selected>Serie</option>
+                        <option value="">9 ano</option>
+                    </select>
+                </div>
+                <div class="col-2">
+                    <FilterQuestionSelect />
+                </div>
+                <div class="col">
+                    <button class="col btn btn-primary">Filtrar</button>
+                </div>
+            </div>
+    </Container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
--- pages/dashboard/questions/index.vue
+++ pages/dashboard/questions/index.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
+        <filter-question/>
         <CardQuestion />
     </div>
 </template>
 <script>
+import FilterQuestion from '~/components/molecules/FilterQuestion.vue';
 export default {
+  components: { FilterQuestion },
     layout: 'Dashboard',
     middleware: 'auth', 
     created() {
         this.$store.dispatch("question/index");
     },
+
+
+    
+
 }
 </script>
