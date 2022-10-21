24ffcdae29c0e31268a548d0017c94984a3137cb
Kevin Almeida <janioklalmeida05@gmail.com>
2022-10-21 19:33:24 -0300

    ✨ feat: new structure for pages

---
+++ pages/quiz/select/country.vue
@@ -0,0 +1,33 @@
+<template>
+  <Container>
+    <div class="quiz-select-country">
+      {{ $testApi }}
+      <h1>Escolha o Continente:</h1>
+      <div v-for="country in countries" :key="country" class="m-4">
+        <QuizCountryCard to="/login" :text="country.name" :src="country.href" />
+      </div>
+    </div>
+  </Container>
+</template>
+<script>
+export default {
+  name: 'QuizSelectCountry',
+  data() {
+    return {
+      countries: [
+        { name: 'América', href: 'america' },
+        { name: 'Europa', href: 'europa' },
+        { name: 'África', href: 'africa' },
+        { name: 'Ásia', href: 'asia' },
+        { name: 'Oceania', href: 'oceania' },
+      ],
+    }
+  },
+  created() {
+    const testApi = this.$axios.$get('http://jifs.freetzi.com/api/student/index.php')
+    console.log(testApi)
+  },
+}
+</script>
+<style scoped>
+</style>
