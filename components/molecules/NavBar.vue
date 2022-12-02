54a4cb793351ec2d571a40bcbc1613eac1bdf49d
Kevin Almeida <janioklalmeida05@gmail.com>
2022-12-02 02:18:20 -0300

    ♻️ refactor: new navbar of bootstrap-vue, and adaptations to the header

--- components/molecules/NavBar.vue
+++ components/molecules/NavBar.vue
@@ -1,94 +1,188 @@
 <template>
-  <div id="nav-bar">
-    <ul
-      v-if="!$auth.loggedIn"
-      id="nav-bar-items"
-      class="
-        mx-auto
-        m-md-0
-        flex-column-reverse
-        align-items-center
-        flex-sm-row
-        nav nav-pills
-        align-items-sm-baseline
-      "
-    >
-      <NavItem text="Início" to="#" />
-      <NavItem text="Sobre" to="#" />
-      <NavItem text="Contato" to="#" />
-      <li
-        v-if="variant === 'student'"
-        id="teacher"
-        class="nav-item modify-type-user"
-        @click="onClick()"
+  <div>
+    <b-navbar v-if="!$auth.loggedIn" toggleable="lg" type="dark">
+      <a
+        class="
+          d-none d-sm-flex
+          align-items-center
+          mb-3 mb-md-0
+          me-md-auto
+          text-decoration-none
+        "
       >
-        <a href="#" class="nav-link d-inline-block">Sou Professor</a>
-      </li>
-      <li
-        v-if="variant === 'teacher'"
-        id="student"
-        class="nav-item modify-type-user"
-        @click="onClick()"
+        <img
+          id="logo-img"
+          src="~/assets/images/logo.png"
+          class="logo"
+          alt="Logo Mini Mundo"
+        />
+      </a>
+      <b-navbar-brand href="#">Mini Mundo</b-navbar-brand>
+      
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="fw-light" href="#">Sobre</b-nav-item>
+          <b-nav-item class="fw-light" href="#">Central de Ajuda</b-nav-item>
+          <b-dropdown
+            id="dropdown-form"
+            ref="dropdown"
+            size="sm"
+            text="Sou Professor"
+            class="m-2"
+            variant="primary"
+          >
+              <b-dropdown-form>
+                <ValidationObserver ref="form" tag="div" class="vee-validation-field">
+                  <b-form-group
+                    label="Email"
+                    label-for="dropdown-form-email"
+                    @submit.stop.prevent
+                  >
+                  <ValidationProvider
+                    v-slot="{ errors, classes}"
+                    tag="div"
+                    rules="required|email"
+                  >
+                    <b-form-input
+                      id="dropdown-form-email"
+                      v-model="credentials.email"
+                      :class="classes"
+                      size="sm"
+                      placeholder="email@example.com"
+                    ></b-form-input>
+                    <span class="error-validation mt-2 ml-2">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                  </b-form-group>
+
+                  <b-form-group label="Senha" label-for="dropdown-form-password">
+                    <ValidationProvider v-slot="{ errors, classes }" tag="div" rules="required">
+                      <b-form-input
+                        id="dropdown-form-password"
+                        v-model="credentials.password"
+                        :class="classes"
+                        type="password"
+                        size="sm"
+                        placeholder="Password"
+                      ></b-form-input>
+                      <span class="error-validation mt-2 ml-2">{{ errors[0] }}</span>
+                    </ValidationProvider>
+                  </b-form-group>
+                </ValidationObserver>
+                <b-button id="form-submit" variant="primary" size="sm" @click="login()"
+                  >Entrar</b-button
+                >
+                <b-dropdown-divider></b-dropdown-divider>
+                <b-dropdown-text>Faça login com as suas credenciais de Professor</b-dropdown-text>
+              </b-dropdown-form>
+          </b-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <b-navbar v-if="$auth.loggedIn" toggleable="lg" type="dark">
+      <a
+        class="
+          d-none d-sm-flex
+          align-items-center
+          mb-3 mb-md-0
+          me-md-auto
+          text-decoration-none
+        "
       >
-        <a href="#" class="nav-link d-inline-block">Sou Aluno</a>
-      </li>
-    </ul>
-    <ul
-      v-if="$auth.loggedIn"
-      id="nav-bar-items"
-      class="
-        mx-auto
-        m-md-0
-        flex-column-reverse
-        align-items-center
-        flex-sm-row
-        nav nav-pills
-        align-items-sm-baseline
-      "
-    >
-      <li
-        id="logout"
-        class="nav-item modify-type-user"
-        @click="$bvModal.show('logoutModal')"
-      >
-        <a href="#" class="nav-link d-inline-block">Sair</a>
-      </li>
-      <b-modal id="logoutModal" hide-backdrop hide-footer hide-header-close>
-        <template #modal-title>
-          <p class="text-body font-weight-bold m-0">
-            Deseja realizar o logout ?
-          </p>
-        </template>
-        <div class="d-block">
-          <p class="text-body">
-            Todos os dados não salvos, serão desconsiderados.
-          </p>
-        </div>
-        <div class="d-flex justify-content-end">
-          <div class="btn btn-light m-1" @click="$bvModal.hide('logoutModal')">
-            Cancelar
-          </div>
-          <div class="btn btn-danger m-1" @click="logout">Sair</div>
-        </div>
-      </b-modal>
-    </ul>
+        <img
+          id="logo-img"
+          src="~/assets/images/logo.png"
+          class="logo"
+          alt="Logo Mini Mundo"
+        />
+      </a>
+      <b-navbar-brand href="#">Mini Mundo</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="fw-light" href="#">Sobre</b-nav-item>
+          <b-nav-item class="fw-light" href="#">Contato</b-nav-item>
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em class="username">{{ $auth.user.firstName }}</em>
+              <i class="text-light fa-regular fa-user"></i>
+            </template>
+            <b-dropdown-item href="#">Conta</b-dropdown-item>
+            <b-dropdown-item href="#">Central de Ajuda</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item href="#" @click="$bvModal.show('logoutModal')"
+              >Sair</b-dropdown-item
+            >
+            <b-modal
+              id="logoutModal"
+              hide-backdrop
+              hide-footer
+              hide-header-close
+            >
+              <template #modal-title>
+                <p class="text-body font-weight-bold m-0">
+                  Deseja realizar o logout ?
+                </p>
+              </template>
+              <div class="d-block">
+                <p class="text-body">
+                  Todos os dados não salvos, serão desconsiderados.
+                </p>
+              </div>
+              <div class="d-flex justify-content-end">
+                <div
+                  class="btn btn-light m-1"
+                  @click="$bvModal.hide('logoutModal')"
+                >
+                  Cancelar
+                </div>
+                <div class="btn btn-danger m-1" @click="logout">Sair</div>
+              </div>
+            </b-modal>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
-<script>
+  <script>
 import ToastMixin from '~/mixins/toastMixin'
 
 export default {
   name: 'NavBar',
   mixins: [ToastMixin],
-  props: {
-    variant: {
-      type: String,
-      default: null,
-    },
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  computed: {
+    user() {
+      return this.$auth.$storage.getUniversal('user');
+    }
   },
   methods: {
-    onClick() {
-      this.$emit('modifyTypeUser')
+    login() {
+      this.$auth
+        .loginWith('local', { data: this.credentials })
+        .then(() => {
+          this.showToastMixin('Seja Bem vindo!', 'Sucesso', 'success')
+        })
+        .catch((err) => {
+          if (err.response.data.errors[0].message === "E_INVALID_AUTH_PASSWORD: Password mis-match") {
+            this.showToastMixin('Verifique suas credenciais!', 'Erro no Login', 'warning')
+          }
+        })
     },
     logout() {
       this.$store.dispatch('user/logout').then(() => {
@@ -102,19 +196,18 @@ export default {
   },
 }
 </script>
-<style scoped>
-.nav-link {
-  font-weight: 500;
-  margin-left: 15px;
-  margin-right: 15px;
-  color: white;
-  border-bottom: solid 4px transparent;
+  <style scoped>
+.logo {
+  width: 70%;
 }
 
-.nav-link:hover {
-  border-bottom: solid 4px var(--primary-color);
-  color: w;
-  transition: 0.3s all;
+.username {
+  font-family: poppinslight !important;
+  color: whitesmoke !important;
+}
+
+.vee-validation-field {
+  width: 100%;
 }
 
 li {
@@ -122,20 +215,15 @@ li {
   margin-bottom: 0px !important;
 }
 
-.modify-type-user {
-  border: 2px solid var(--primary-color);
-  color: white;
-  padding: 5px;
-  border-radius: 15px;
+.is-valid {
+  border: 1px solid #00c9a7;
 }
 
-.modify-type-user:hover {
-  background-color: var(--primary-color);
-  transition: 0.3s all;
+.is-invalid {
+  border: 1px solid #ff0000;
 }
 
-.modify-type-user a:hover {
-  border-bottom: solid 4px white !important;
-  transition: 0.3s all;
+.error-validation {
+  color: #ff0000;
 }
 </style>
--- components/templates/DashboardTemplate.vue
+++ components/templates/DashboardTemplate.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <Header />
-    <DashBar />
-  </div>
-</template>
-<script>
-export default {
-  name: 'DashboardTemplate',
-}
-</script>
-<style>
-</style>
+    <div>
+      <Header />
+      <DashBar />
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'DashboardTemplate',
+  }
+  </script>
+  <style>
+  </style>
