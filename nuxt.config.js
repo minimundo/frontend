a94bb1f7831484770beacd7f39506e6863c52c94
Kevin Almeida <janioklalmeida05@gmail.com>
2022-12-04 17:53:59 -0300

    🔧 chore: new axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://api.minimundo.online/api',
+    baseURL: 'https://api.minimundo.online/api',
   },
 
   auth: {
