fa27af6dc85c738e9c5bf99e68205b3e39b2b2fc
Kevin Almeida <janioklalmeida05@gmail.com>
2023-01-25 12:24:31 -0300

    ✨ feat: new layout to cardQuiz

--- components/organisms/CardQuiz.vue
+++ components/organisms/CardQuiz.vue
@@ -1,78 +1,78 @@
 <template>
   <div class="container mt-5">
-    <div class="d-flex justify-content-center row">
+    <div v-if="viewCard === true" class="d-flex justify-content-center row">
       <div class="col-md-10 col-lg-10">
         <div class="border">
-          <div class="question bg-white p-3 border-bottom">
+          <div class="question bg-primary p-3 border-bottom">
             <div
               class="
                 d-flex
                 flex-row
                 justify-content-between
                 align-items-center
-                mcq
+                text-light
               "
             >
-              <h4 class="text-dark">MCQ Quiz</h4>
-              <span class="text-dark"
+              <div></div>
+              <h4>
+                {{ $store.state.quiz.intro.grade }}º -
+                {{ $store.state.quiz.intro.country.name }}
+              </h4>
+              <span
                 >({{ currentQuestion + 1 }} de {{ $questionsQuizCount }})</span
               >
-            </div>
-          </div>
-          <div
-            v-if="$questionsQuiz[currentQuestion] != undefined"
-            class="question bg-white p-3 border-bottom"
-          >
-            <div class="d-flex flex-row align-items-center question-title">
-              <h5 class="mt-1 ml-2 text-dark">
-                {{ $questionsQuiz[currentQuestion].wording }}
-              </h5>
-            </div>
-            <div
-              class="d-flex flex-row align-items-center question-title"
-            ></div>
-            <div class="ans ml-2">
-              <label class="radio">
-                <input v-model="answer" type="radio" name="answer" value="1" />
-                <span>{{ $questionsQuiz[currentQuestion].answer1 }}</span>
-              </label>
-            </div>
-            <div class="ans ml-2">
-              <label class="radio">
-                <input v-model="answer" type="radio" name="answer" value="2" />
-                <span>{{ $questionsQuiz[currentQuestion].answer2 }}</span>
-              </label>
-            </div>
-            <div class="ans ml-2">
-              <label class="radio">
-                <input v-model="answer" type="radio" name="answer" value="3" />
-                <span>{{ $questionsQuiz[currentQuestion].answer3 }}</span>
-              </label>
-            </div>
-            <div class="ans ml-2">
-              <label class="radio">
-                <input v-model="answer" type="radio" name="answer" value="4" />
-                <span>{{ $questionsQuiz[currentQuestion].answer4 }}</span>
-              </label>
             </div>
           </div>
           <div
             class="
               d-flex
               flex-row
-              justify-content-between
               align-items-center
-              p-3
+              question-title question
               bg-white
+              p-3
+              border-bottom
             "
           >
+            <h5
+              v-if="$questionsQuiz[currentQuestion]"
+              class="mt-1 ml-2 text-dark"
+            >
+              {{ $questionsQuiz[currentQuestion].wording }}
+            </h5>
+          </div>
+          <div
+            v-if="$questionsQuiz[currentQuestion]"
+            class="question d-flex flex-column bg-white p-3 border-bottom"
+          >
+            <div class="col-sm-10 col-12">
+              <label class="radio">
+                <input v-model="answer" type="radio" name="answer" value="1" />
+                <span>{{ $questionsQuiz[currentQuestion].answer1 }}</span>
+              </label>
+            </div>
+            <div class="col-sm-10 col-12">
+              <label class="radio">
+                <input v-model="answer" type="radio" name="answer" value="2" />
+                <span>{{ $questionsQuiz[currentQuestion].answer2 }}</span>
+              </label>
+            </div>
+            <div class="col-sm-10 col-12">
+              <label class="radio">
+                <input v-model="answer" type="radio" name="answer" value="3" />
+                <span>{{ $questionsQuiz[currentQuestion].answer3 }}</span>
+              </label>
+            </div>
+            <div class="col-sm-10 col-12">
+              <label class="radio">
+                <input v-model="answer" type="radio" name="answer" value="4" />
+                <span>{{ $questionsQuiz[currentQuestion].answer4 }}</span>
+              </label>
+            </div>
+          </div>
+          <div class="d-flex flex-row justify-content-end p-3 bg-white">
             <button
-              class="
-                btn btn-primary
-                border-success
-                align-items-center
-                btn-success
-              "
+              class="btn btn-primary border-success btn-success"
               type="button"
               @click="nextQuestion"
             >
@@ -82,6 +82,20 @@
         </div>
       </div>
     </div>
+    <b-modal id="resultModal" hide-backdrop hide-footer hide-header-close>
+      <div class="card">
+        <div class="card-body text-center text-dark">
+          <img src="https://img.icons8.com/bubbles/200/000000/trophy.png" />
+          <h4>PARABÉNS!</h4>
+          <p>
+            Você acertou {{ $store.state.quiz.playerResult.correct }}/{{
+              $questionsQuizCount
+            }}
+          </p>
+          <button class="btn btn-out btn-square continue" @click="$router.push('/quiz/intro')">CONTINUE</button>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
@@ -93,6 +107,7 @@ export default {
     return {
       currentQuestion: 0,
       answer: null,
+      viewCard: true
     }
   },
   computed: {
@@ -122,7 +137,11 @@ export default {
     this.$store
       .dispatch('question/search', payload)
       .then((response) => {
-        this.$store.dispatch('quiz/init', response)
+        this.$store.dispatch('quiz/init', response).then(() => {
+          if (response.length === 0) {
+            this.$router.push('/quiz/intro')
+          }
+        })
       })
       .catch(() => {
         this.showToastMixin(
@@ -147,7 +166,9 @@ export default {
       }
 
       if (this.currentQuestion >= this.$questionsQuizCount) {
-        this.$router.push('/quiz/game/results')
+        this.$store.commit('quiz/SET_PLAYER_RESULT')
+        this.viewCard = false
+        this.$bvModal.show('resultModal')
       }
     },
   },
@@ -171,27 +192,21 @@ label.radio input {
 }
 
 label.radio span {
-  padding: 4px 0px;
-  border: 1px solid red;
+  padding: 4px 2px;
+  border: 0.5px solid var(--second-color);
   display: inline-block;
-  color: red;
-  width: 100px;
-  text-align: center;
+  color: black;
+  width: 100%;
+  text-align: left;
   border-radius: 3px;
   margin-top: 7px;
-  text-transform: uppercase;
 }
 
 label.radio input:checked + span {
-  border-color: red;
-  background-color: red;
+  border-color: var(--second-color);
+  background-color: var(--second-color);
   color: #fff;
 }
-
-.ans {
-  margin-left: 36px !important;
-}
-
 .btn:focus {
   outline: 0 !important;
   box-shadow: none !important;
@@ -200,5 +215,77 @@ label.radio input:checked + span {
 .btn:active {
   outline: 0 !important;
   box-shadow: none !important;
+}
+
+@import url(http://fonts.googleapis.com/css?family=Calibri:400,300,700);
+
+body {
+  background-color: #d32f2f;
+  font-family: 'Calibri', sans-serif !important;
+}
+
+.mt-100 {
+  margin-top: 100px;
+}
+
+.container {
+  margin-top: 200px;
+}
+
+.card {
+  position: relative;
+  display: flex;
+  width: 450px;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid #d2d2dc;
+  border-radius: 4px;
+  -webkit-box-shadow: 0px 0px 5px 0px rgb(249, 249, 250);
+  -moz-box-shadow: 0px 0px 5px 0px rgba(212, 182, 212, 1);
+  box-shadow: 0px 0px 5px 0px rgb(161, 163, 164);
+}
+
+.card .card-body {
+  padding: 1rem 1rem;
+}
+
+.card-body {
+  flex: 1 1 auto;
+  padding: 1.25rem;
+}
+
+p {
+  font-size: 14px;
+}
+
+h4 {
+  margin-top: 18px;
+}
+
+.cross {
+  padding: 10px;
+  color: #d6312d;
+  cursor: pointer;
+}
+
+.continue:focus {
+  outline: none;
+}
+
+.continue {
+  border-radius: 5px;
+  text-transform: capitalize;
+  font-size: 13px;
+  padding: 8px 19px;
+  cursor: pointer;
+  color: #fff;
+  background-color: #d50000;
+}
+
+.continue:hover {
+  background-color: #d32f2f !important;
 }
 </style>
